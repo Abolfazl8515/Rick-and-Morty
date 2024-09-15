38573410bae1c6a79de59e98bcfd4aac507efaca
Abolfazl <boorbooryabolfazl85@gmail.com>
2024-09-16 01:20:11 +0330

    browserrouter changed to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { store } from "./store.js";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
